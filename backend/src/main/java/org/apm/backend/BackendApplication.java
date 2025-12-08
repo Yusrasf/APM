465dbbe79584e549934af18a09ac3129725936d1
stefaniaboldeanu15 <stefaniaboldeanu15@gmail.com>
2025-12-09 01:43:01 +0200

    Update BackendApplication
    removed Jpa dependencies - the app does not have a DB

--- backend/src/main/java/org/apm/backend/BackendApplication.java
+++ backend/src/main/java/org/apm/backend/BackendApplication.java
@@ -2,20 +2,14 @@ package org.apm.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@SpringBootApplication(
-        exclude = {
-                DataSourceAutoConfiguration.class,
-                HibernateJpaAutoConfiguration.class,
-                UserDetailsServiceAutoConfiguration.class
-        }
-)
+@SpringBootApplication
+@ServletComponentScan
 public class BackendApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BackendApplication.class, args);
     }
 }
